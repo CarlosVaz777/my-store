8ce8975706d42c8105093aa69caa1e50bb18ef87
Carlos Vazquez <carlos.enrique.3em@gmail.com>
2025-01-22 23:43:59 -0600

    Se creo el componente, Productos y FQAs, creación de rutas

---
+++ src/app/faqs/faqs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-faqs',
+  imports: [RouterModule, RouterOutlet],
+  templateUrl: './faqs.component.html',
+  styleUrl: './faqs.component.css'
+})
+export class FaqsComponent {
+
+}
